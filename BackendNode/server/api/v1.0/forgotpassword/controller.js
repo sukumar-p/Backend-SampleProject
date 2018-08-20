03992274d2dce9d2c99752ad4f1b3e60ca96e6e6
chanti123 <sukumar.p@mtwlabs.com>
2018-08-20 12:12:06 +0530

    updatepassword

--- BackendNode/server/api/v1.0/forgotpassword/controller.js
+++ BackendNode/server/api/v1.0/forgotpassword/controller.js
@@ -1,4 +1,5 @@
-let forgotpasswordcollection = require('../signup/model')
+let forgotpasswordcollection = require('../signup/model');
+let updatepasswordcollection = require("../signup/model");
 let mongoose = require('mongoose');
 
 
@@ -17,84 +18,23 @@ let newpassword = (req, res) =>
                     // res.status(400).json({status: false,message: "user already exists"})
 
 
-                    console.log("hai",response);
+                    console.log("hai", response);
 
-                    var html = "hello world! your email verification link is <a href='http://localhost:3000/v1/create/verify/" + response.hashPassword + "'>click here to verify</a>";
-                     console.log("resp",html)
-                    //  const nodemailer = require('nodemailer');
-                    // const transport = nodemailer.createTransport({
-                    //     service: 'Gmail',
-                    //     auth: {
-                    //         user: 'p.sukumara7@gmail.com',
-                    //         pass: 'idieforindia1',
-                    //     }
-                    // });
-                    // const mailOptions = {
-                    //     from: "p.sukumara7@gmail.com",
-                    //     to: "vipinreddy8@gmail.com",
-                    //     subject: 'hello world!',
-                    //     html: html,
-                    // };
-                    // transport.sendMail(mailOptions, (error, info) => {
-                    //     if (error) {
-                    //         console.log(error);
-                    //     }
-                    //     else {
-                    //         console.log(`Message sent: ${info.response}`);
-                    //         res.status(200).json({ status: true, message: "user successfully created and password send to your email to login please verify your email" });
-                    //     }
-
-                    // });
-
-
-
-                    // const nodemailer= require("nodemailer");
-                    // nodemailer.createTestAccount((err, account) => {
-                    //     // create reusable transporter object using the default SMTP transport
-                       
-                    //     let transporter = nodemailer.createTransport({
-                    //         host: 'smtp.gmail.com',
-                    //         port: 587,
-                    //         secure: false, // true for 465, false for other ports
-                    //         auth: {
-                    //             user: account.user, // generated ethereal user
-                    //             pass: account.pass // generated ethereal password
-                    //         }
-                    //     });
-                    
-                    //     // setup email data with unicode symbols
-                    //     let mailOptions = {
-                    //         from: 'p.sukumara7@gmail.com', // sender address
-                    //         to: 'vipinreddy8@gmail.com', // list of receivers
-                    //         subject: 'Hello ✔', // Subject line
-                    //         text: 'Hello world?', // plain text body
-                    //         html: html // html body
-                    //     };
-                    
-                    //     // send mail with defined transport object
-                    //     transporter.sendMail(mailOptions, (error, info) => {
-                    //         if (error) {
-                    //             return console.log(error);
-                    //         }
-                    //         console.log('Message sent: %s', info.messageId);
-                    //         // Preview only available when sending through an Ethereal account
-                    //         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-                    
-                    //         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-                    //         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-                    //     });
-                    // });
+                    var html = "hello world! your email verification link is <a href='http://localhost:4200/resetpassword/" + response.id + "'>click here to verify</a>";
 
 
 
 
-
-
-
-
-
-
-
+                    const sgMail = require('@sendgrid/mail');
+                    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+                    const msg = {
+                        to: response.email,
+                        from: 'p.sukumara7@gmail.com',
+                        subject: 'Sending with SendGrid is Fun',
+                        text: 'and easy to do anywhere, even with Node.js',
+                        html: html
+                    };
+                    sgMail.send(msg);
 
                 } else {
                     res.status(500).json({ status: false, message: "user does not exist" })
@@ -122,6 +62,19 @@ let newpassword = (req, res) =>
 //     )
 // }
 
+
+
+let updatepassword = (req, res) => {
+    console.log(req.body);
+    updatepasswordcollection.findOneAndUpdate({ email: req.body.email }, { $set: { password: req.body.password } })
+        .then(
+            (response) => {
+                console.log(response)
+            }
+        )
+
+}
+
 module.exports = {
-    newpassword
+    newpassword, updatepassword
 }
--- BackendNode/server/api/v1.0/forgotpassword/route.js
+++ BackendNode/server/api/v1.0/forgotpassword/route.js
@@ -2,6 +2,7 @@ let express=require('express')
 let router=express.Router();
 let passwordresetcontroller = require("./controller")
 
-router.post('/passwordreset',passwordresetcontroller.newpassword)
+router.post('/passwordreset',passwordresetcontroller.newpassword);
+router.post('/passwordreset',passwordresetcontroller.updatepassword)
 
 module.exports=router;
